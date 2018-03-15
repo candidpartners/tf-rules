862333dbb16029601b7a9245c38f9a4c2cadd035
Ethan Bower <>
2018-03-15 14:02:40 -0400

    Updated names

--- src/publish-to-npm.sh
+++ src/publish-to-npm.sh
@@ -7,8 +7,8 @@ if [ "$?" = "0" ]; then
     git ls-files --modified --deleted
     version=$(cat ./package.json | jq '.version')
 
-    echo Publishing @CandidPartners/snitch ${version}
-    npm publish --access public
+    echo Publishing snitch ${version}
+    npm publish --access private
 
     echo Tagging Git Repo with version $version
     git tag -a ${version} -m ${version}
