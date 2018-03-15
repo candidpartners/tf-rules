f90ca150b4611a10aca214a0464dd56fdbcce958
Ethan Bower <>
2018-03-15 12:04:51 -0400

    renamed tf-rules to snitch

--- tf-rules/src/lib/rules/kms/aws-kms-ensure-key-rotation/aws-kms-ensure-key-rotation.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_kms_key" "my_key" {
-  description             = "KMS key 1"
-  deletion_window_in_days = 10
-  enable_key_rotation = true
-}
--- tf-rules/ui/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-npm run build
-
-aws s3 sync ./build s3://tf-rules-website --delete
