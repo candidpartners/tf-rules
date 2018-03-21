e8100db73a186302a9f0f4f25376e011efb96254
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-03-21 14:27:28 -0400

    AWS Config Lambda Livecheck working

--- aws-config-lambda/src/index.js
+++ aws-config-lambda/src/index.js
@@ -1,34 +1,39 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
-const config = new AWS.ConfigService();
-const {Snitch,Rules} = require('@candidpartners/snitch');
+const co = require('co');
+const AWSConfig = new AWS.ConfigService();
+const Snitch = require('@candidpartners/snitch');
 
 // Receives the event and context from AWS Lambda.
-exports.handler = (event, context, callback) => {
+exports.handler = co.wrap(function * (event, context, callback){
     console.log(event);
     let {resultToken,accountId} = event;
 
-    Snitch.livecheck({provider: AWS, rules: Rules,config: {}})
-        .then(result => {
-            console.log({result})
-        })
-        .catch(console.error)
-        .then(() => {
-            let params = {
-                ResultToken: resultToken,
-                Evaluations: [
-                    {
-                        ComplianceResourceId: accountId, //'STRING_VALUE', /* required */
-                        ComplianceResourceType: "AWS::::Account", //'STRING_VALUE', /* required */
-                        ComplianceType: "NON_COMPLIANT", //COMPLIANT | NON_COMPLIANT | NOT_APPLICABLE | INSUFFICIENT_DATA, /* required */
-                        OrderingTimestamp: new Date(), //|| 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789, /* required */
-                        Annotation: 'This account is broken.'
-                    },
-                ],
-            };
+    try{
+        let config = Snitch.LoadConfigFromFile(__dirname + "/snitch.config.yml");
+        let result = yield Snitch.Livecheck({provider: AWS,config});
+        let failedResults = result.filter(x => x.valid == 'fail');
 
-            config.putEvaluations(params).promise()
-                .then(data => callback(null,data))
-                .catch(err => callback(err))
-        });
-};
+        let isCompliant = failedResults.length == 0;
+        let Annotation = JSON.stringify(failedResults.map(x => x.message),null,2);
+
+        let params = {
+            ResultToken: resultToken,
+            Evaluations: [
+                {
+                    ComplianceResourceId: accountId, //'STRING_VALUE', /* required */
+                    ComplianceResourceType: "AWS::::Account", //'STRING_VALUE', /* required */
+                    ComplianceType: (isCompliant) ? "COMPLIANT" : "NON_COMPLIANT", //"NON_COMPLIANT", //COMPLIANT | NON_COMPLIANT | NOT_APPLICABLE | INSUFFICIENT_DATA, /* required */
+                    OrderingTimestamp: new Date(), //|| 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789, /* required */
+                    Annotation
+                },
+            ],
+        };
+
+        console.log(params);
+        let putEvaluationsResult = yield AWSConfig.putEvaluations(params).promise();
+        callback(null,{params,putEvaluationsResult});
+    } catch (err) {
+        callback(err)
+    }
+});
