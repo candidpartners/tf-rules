91ae435a9ab4e2d986aa3c6a511e5d7b0e4c9115
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-03-26 12:59:05 -0400

    Got periodic rule working for AWS::EC2::Instance

--- aws-config-lambda/src/index.js
+++ aws-config-lambda/src/index.js
@@ -1,17 +1,25 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
 const co = require('co');
+const _ = require('lodash');
 const AWSConfig = new AWS.ConfigService();
 const Snitch = require('@candidpartners/snitch');
 
 // Receives the event and context from AWS Lambda.
 exports.handler = co.wrap(function * (event, context, callback){
     console.log(event);
-    let {resultToken,accountId} = event;
+    let {resultToken,accountId,invokingEvent} = event;
+
+    // Check if it is a triggered event
+    let config_trigger =_.get(invokingEvent,'configurationItem.resourceType',undefined);
 
     try{
         let config = Snitch.LoadConfigFromFile(__dirname + "/snitch.config.yml");
-        let result = yield Snitch.Livecheck({provider: AWS, config});
+        let result = yield Snitch.Livecheck({
+            provider: AWS,
+            config,
+            config_triggers: config_trigger ? [config_trigger] : []
+        });
         let failedResults = result.filter(x => x.valid == 'fail');
 
         let isCompliant = failedResults.length == 0;
