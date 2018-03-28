386f15e198d42bda749c5b564e07f84d655f004e
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-03-28 15:05:24 -0400

    Bumped version

--- pipeline/publish-to-npm.sh
+++ pipeline/publish-to-npm.sh
@@ -7,7 +7,7 @@ git commit -a -m "Bumped version"
 version=$(cat ./package.json | jq '.version')
 git tag -a ${version} -m ${version}
 git push
-npm publish --access private
+npm publish --access restricted
 
 
 # Check if any changes are pending
