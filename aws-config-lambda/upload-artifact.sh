98b6fb5c664cec3eb8a485b61000b86ccefe7aff
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2018-04-03 15:07:50 -0400

    Added noncompliant_resources to cloutrail-bucket-is-not-publicly-accessible

--- aws-config-lambda/upload-artifact.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-aws s3 cp ./snitch_deployment.zip s3://volker-static-assets/snitch_deployment.zip
-rm ./snitch_deployment.zip
--- src/lib/rules/s3/aws-s3-cloudtrail-bucket-is-not-publicly-accessible/aws-s3-cloudtrail-bucket-is-not-publicly-accessible.js
+++ src/lib/rules/s3/aws-s3-cloudtrail-bucket-is-not-publicly-accessible/aws-s3-cloudtrail-bucket-is-not-publicly-accessible.js
@@ -53,7 +53,14 @@ S3CloudTrailBucketIsNotPubliclyAccessible.livecheck = co.wrap(function* (context
         if (err.code === 'AccessDenied') {
             return new RuleResult({
                 valid: "fail",
-                message: "Snitch does not have access to the CloudTrail S3 bucket from this account."
+                message: "Snitch does not have access to the CloudTrail S3 bucket from this account.",
+                noncompliant_resources: [
+                    new NonCompliantResource({
+                        resource_id: "Permission Error",
+                        resource_type: "AWS::::Account",
+                        message: "Snitch does not have access to the CloudTail S3 bucket from this account."
+                    })
+                ]
             })
         }
     }
