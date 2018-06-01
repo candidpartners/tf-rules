0289dede6abd08ffb07676463822431c88dc2ce3
rrawla <rrawla@gmail.com>
2018-05-31 22:15:40 -0400

    fix minor bug

---
+++ src/lib/rules/config-service/aws-config-service-is-enabled-in-current-region/validate.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk');
+let rule = require('./aws-config-service-is-enabled-in-current-region');
+let provider=AWS;
+
+provider.config = {region: 'us-east-2'};
+
+const result=rule.livecheck({config:true,provider:provider}).then((r)=>{
+    console.log(JSON.stringify(r));
+});
